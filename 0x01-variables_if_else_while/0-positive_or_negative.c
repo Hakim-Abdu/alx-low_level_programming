c69bce63a2c0b197e73e2645e7d08458d9439273
Hakim-Abdu <hakimabdu555@gmail.com>
2022-07-06 07:14:44 -0700

    0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -17,8 +17,9 @@ if (n > 0)
 printf("%i is positive\n", n);
 else if (n < 0)
 printf("%i is negative\n", n);
-else										printf("%i is zero\n", n);
+else	
 
+printf("%i is zero\n", n);
 return (0);
 
 }
