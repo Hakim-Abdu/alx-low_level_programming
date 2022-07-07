658a98202e4a66a465e76296ca88da6186c4c0f6
Hakim-Abdu <hakimabdu555@gmail.com>
2022-07-07 10:23:49 -0700

    1-main.c

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -22,18 +22,12 @@ int main(void)
 
 
 	/*
-	 *
-	 * *while (i < 10)
-	 *
-	 * *{
-	 *
-	 * *Infinite loop - No increase of variable
-	 *
-	 * *putchar(i);
-	 *
+	 *while (i < 10)
+	 *{
+	 *Infinite loop - No increase of variable
+	 *putchar(i);
 	 * }
-	 *
-	 * */
+	 */
 
 	printf("Infinite loop avoided! \\o/\n");
 
