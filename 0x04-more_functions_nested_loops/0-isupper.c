0dd8d43afee29c011dbef8d2d3d90b4e9ddc5549
Hakim-Abdu <hakimabdu555@gmail.com>
2022-07-07 11:52:56 -0700

    0-isupper.c

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -6,27 +6,14 @@
  * Return: Always 0.
  */
 int _isupper(int c)
-
 {
-
-	if (c >= 'A' && c <= 'Z')
-
-	{
-
-		return (1);
-
-	}
-
-
-
-	else
-
-	{
-
-		return (0);
-
-	}
-
-
+if (c >= 'A' && c <= 'Z')
+{
+return (1);
+}
+else
+{
+return (0);
+}
 
 }
